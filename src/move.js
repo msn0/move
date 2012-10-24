3a727be1c936c567cb42ccc397068605c8d1abec
msn0 <ambinanitelo@gmail.com>
2012-10-24 22:22:32 +0200

    slightly modern approach

---
+++ src/move.js
@@ -0,0 +1,102 @@
+(function(){
+
+  var MovableElement = {
+
+    init: function(elementToMove, handlerElement){
+      var that = this;
+      this.elementToMove = elementToMove;
+      this.handlerElement = handlerElement || elementToMove;
+      this.x = 0;
+      this.y = 0;
+      this.movedX = 0;
+      this.movedY = 0;
+      this.addEvent(this.handlerElement, 'mousedown', this.bind(this, this.mouseDown), true);
+      return this;
+    },
+
+    bind: function(scope, fn) {
+      return function () {
+        fn.apply(scope, arguments);
+      };
+    },
+
+    getEvent: function(e){
+      return e || window.event;
+    },
+
+    calculatePosition: function(e){
+      if (e.pageY){
+        this.x = e.pageX;
+        this.y = e.pageY;
+      }
+      else {
+        this.x = e.clientX;
+        this.y = e.clientY;
+      }
+      return this;
+    },
+
+    setPosition:function(e){
+      var diffX, diffY;
+      this.movedX = e.pageX || e.clientX;
+      this.movedY = e.pageY || e.clientY;
+      diffX = this.x - this.movedX;
+      diffY = this.y - this.movedY;
+      this.calculatePosition(e);
+      this.elementToMove.style.top = this.elementToMove.offsetTop - diffY + "px";
+      this.elementToMove.style.left = this.elementToMove.offsetLeft - diffX + "px";
+      return this;
+    },
+
+    addEvent: function(obj, eventType, eventHandler, useCapture) {
+      useCapture || (useCapture = false);
+      if (obj.addEventListener) {
+        obj.addEventListener(eventType, eventHandler, true);
+      } else if (obj.attachEvent) {
+        obj.attachEvent('on' + eventType, eventHandler);
+      }
+    },
+
+    mouseDown: function(e){
+      e = this.getEvent(e);
+      e.preventDefault();
+      this.mU = this.bind(this, this.mouseUp);
+      this.mM = this.bind(this, this.mouseMove);
+      this.handlerElement.style.cursor = "move";
+      this.addEvent(document, "mouseup", this.mU, false);
+      this.addEvent(document, "mousemove", this.mM, false);
+      this.calculatePosition(e);
+      return false;
+    },
+
+    mouseUp: function(e){
+      e = this.getEvent(e);
+      this.removeEvent(document, "mouseup", this.mU, false);
+      this.removeEvent(document, "mousemove", this.mM, false);
+      this.handlerElement.style.cursor = "default";
+      return false;
+    },
+
+    mouseMove: function(e){
+      e = this.getEvent(e);
+      this.setPosition(e);
+      return false;
+    },
+
+    removeEvent: function(obj, eventType, eventHandler, useCapture) {
+      useCapture || (useCapture = false);
+      if (obj.removeEventListener) {
+        obj.removeEventListener(eventType, eventHandler, true);
+      } else if (obj.attachEvent) {
+        obj.attachEvent('on' + eventType, eventHandler);
+      }
+    },
+  };
+
+  MovableElement.setMovableElement = function(elementToMove, handlerElement){
+    return Object.create(MovableElement).init(elementToMove, handlerElement);
+  };
+
+  window.MovableElement = MovableElement;
+
+})();
